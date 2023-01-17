1abdfe39e6ed182732b6346d31137242f91ba0f2
Changhyung <pck4949@naver.com>
2023-01-17 22:54:15 +0900

    [Map(맵)] 완주하지 못한 선수

--- src/day1/Solution3.java
+++ src/day1/Solution3.java
@@ -1,57 +1,24 @@
 package day1;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Solution3 {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, ArrayList<Boolean>> hashMap = new HashMap<>();
-
-        for (String p : participant) {
-            if (hashMap.getOrDefault(p, null) == null) {
-                ArrayList<Boolean> arrayList = new ArrayList<>();
-                arrayList.add(false);
-                hashMap.put(p, arrayList);
-            } else {
-                hashMap.get(p).add(false);
-            }
-        }
-
-        for (String c : completion) {
-            if (hashMap.get(c).size() > 1) {
-                hashMap.get(c).add(true);
-            } else {
-                hashMap.get(c).set(0, true);
-            }
-        }
-
+        Map<String, Integer> name = new HashMap<>();
+        for (String s : participant)
+            if (name.get(s) != null) // 값이 있으면 +1
+                name.put(s, name.get(s) + 1);
+            else
+                name.put(s, 1); // 처음이면 1
+        for (String s : completion) // 완주한사람에서 1뺀다. 완주한사람은 0이됨
+            name.put(s, name.get(s) - 1);
         String answer = "";
-
-        for (String p : participant) {
-            if (hashMap.get(p).size() > 1) {
-                int trueCount = 0;
-                int falseCount = 0;
-
-                for (boolean b : hashMap.get(p)) {
-                    if (b) {
-                        trueCount++;
-                    } else {
-                        falseCount++;
-                    }
-                }
-                if (trueCount != falseCount) {
-                    answer = p;
-                    break;
-                }
-            } else {
-                if (!hashMap.get(p).get(0)) {
-                    answer = p;
-                    break;
-                }
+        for (Map.Entry<String, Integer> e : name.entrySet())
+            if (e.getValue() > 0) {
+                answer = e.getKey();
+                break;
             }
-        }
-
         return answer;
     }
 }
