65b1f21e2a7ce08e420d63c44190d8a2822591ae
Changhyung <pck4949@naver.com>
2023-01-17 22:57:10 +0900

    [Map(맵)] 완주하지 못한 선수

--- src/day1/Solution3.java
+++ src/day1/Solution3.java
@@ -6,16 +6,14 @@ import java.util.Map;
 public class Solution3 {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> name = new HashMap<>();
-        for (String s : participant)
-            if (name.get(s) != null) // 값이 있으면 +1
-                name.put(s, name.get(s) + 1);
-            else
-                name.put(s, 1); // 처음이면 1
-        for (String s : completion) // 완주한사람에서 1뺀다. 완주한사람은 0이됨
-            name.put(s, name.get(s) - 1);
-        String answer = "";
-        for (Map.Entry<String, Integer> e : name.entrySet())
-            if (e.getValue() > 0) {
+
+        for(String s : participant)
+            name.put(s, name.getOrDefault(s, 0)+1); // boxing
+        for(String s : completion) // 완주한사람에서 1뺀다. 완주한사람은 0이됨
+            name.put(s, name.get(s)-1);
+        String answer ="";
+        for(Map.Entry<String, Integer> e : name.entrySet())
+            if(e.getValue() > 0) {
                 answer = e.getKey();
                 break;
             }
